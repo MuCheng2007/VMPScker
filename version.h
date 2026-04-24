efdbec3d8182f1c89969de1ea21bf11b67ff5d48
qiranhua <qiranhua@gmail.com>
2026-04-25 00:15:22 +0800

    减少无用文件

--- version.h
+++
@@ -1,6 +0,0 @@
-#define VER_MAJOR 3 
-#define VER_MINOR 5 
-#define VER_PATCH 1 
-#define VER_BUILD 1024 
-#define VER_FILE "3.5.1.1024" 
-#define VER_PRODUCT "3.5.1" 
--- win_runtime.Tests/precompiled.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef WRUT_PCH
-#define WRUT_PCH
-
-#include "../runtime/precompiled.h"
-
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
-#endif //WRUT_PCH
